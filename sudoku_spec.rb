256fbc687324baebc66f9989b1e90844391ce48a
razwanizmi <razwan.izmi@gmail.com>
2016-08-15 10:41:44 +0800

    Wrote save empty positions spec

--- sudoku_spec.rb
+++ sudoku_spec.rb
@@ -25,4 +25,20 @@ describe Sudoku do
       expect(parsed_board).to eq(expected_board)
     end
   end
+
+  describe "#save_empty_positions" do
+    it "should save all of the empty positions, 0s, in a parsed board" do
+      empty_positions = my_sudoku.save_empty_positions
+      expected_positions =  [
+                              [0,0],[0,2],[0,3],[0,4],[0,5],[0,6],[0,7],[1,0],[1,1], 
+                              [1,2],[1,5],[2,0],[2,1],[2,2],[2,6],[2,7],[2,8],[3,0], 
+                              [3,1],[3,3],[3,4],[3,5],[3,6],[3,7],[3,8],[4,4],[4,5], 
+                              [4,7],[4,8],[5,1],[5,3],[5,4],[5,5],[5,8],[6,1],[6,2], 
+                              [6,4],[6,6],[6,8],[7,1],[7,3],[7,4],[7,5],[7,8],[8,1], 
+                              [8,2],[8,3],[8,5],[8,6],[8,7],[8,8]
+                            ]
+      expect(empty_positions.length).to eq(51)
+      expect(empty_positions).to eq(expected_positions)
+    end
+  end
 end
