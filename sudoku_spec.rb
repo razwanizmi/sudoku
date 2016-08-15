849bb41b9eb1a2a4b57e44bb17e9ed22e7d85a7e
razwanizmi <razwan.izmi@gmail.com>
2016-08-15 13:44:34 +0800

    Write spec for solve puzzle

--- sudoku_spec.rb
+++ sudoku_spec.rb
@@ -105,4 +105,22 @@ describe Sudoku do
       end
     end
   end
+
+  describe "#solve_puzzle" do
+    it "returns a solution to the puzzle passed in" do
+      empty_positions = my_sudoku.save_empty_positions(parsed_board)
+      expected_solution = [
+                            [ 8,9,5,7,4,2,1,3,6 ], 
+                            [ 2,7,1,9,6,3,4,8,5 ], 
+                            [ 4,6,3,5,8,1,7,9,2 ], 
+                            [ 9,3,4,6,1,7,2,5,8 ], 
+                            [ 5,1,7,2,3,8,9,6,4 ], 
+                            [ 6,8,2,4,5,9,3,7,1 ], 
+                            [ 1,5,9,8,7,4,6,2,3 ], 
+                            [ 7,4,6,3,2,5,8,1,9 ], 
+                            [ 3,2,8,1,9,6,5,4,7 ]
+                          ]
+      expect(my_sudoku.solve_puzzle(parsed_board, empty_positions)).to eq(expected_solution)
+    end
+  end
 end
